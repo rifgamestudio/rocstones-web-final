8b4814075e1cd7f174ca8f50cc219a0dbb82f145
rafik <rifgamestudio@gmail.com>
2026-01-17 03:43:21 +0100

    pagina forzada al fr

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,22 @@
 import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
-  // Lista actualizada con los 5 idiomas
+  // Lista con los 5 idiomas
   locales: ['fr', 'en', 'es', 'de', 'nl'],
+  
+  // Idioma por defecto absoluto
   defaultLocale: 'fr',
-  localePrefix: 'always'
+  
+  // Fuerza a que siempre aparezca el prefijo /fr/
+  localePrefix: 'always',
+
+  // DESACTIVA LA DETECCIÓN AUTOMÁTICA: 
+  // Esto obliga a Google a entrar siempre por el francés 
+  // en lugar de intentar adivinar el idioma.
+  localeDetection: false 
 });
  
 export const config = {
-  // Matcher actualizado para reconocer las nuevas rutas /de/ y /nl/
+  // Matcher actualizado
   matcher: ['/', '/(fr|en|es|de|nl)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)']
 };
